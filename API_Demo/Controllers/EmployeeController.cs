80f326319865b3fb96d70fb44fc28358f88c63ba
Prakash Kumar <EE210673@sasken.com>
2019-05-08 11:44:56 +0530

    Pagination

---
+++ API_Demo/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using API_Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace API_Demo.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        private int rowCount = 1;
+        private List<SampleEmployee> employees = new List<SampleEmployee>();
+        public string connection = @"Data Source=PCZ-EE210673\SQLEXPRESS;Initial Catalog=StudentDB;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework";
+        public List<SampleEmployee> GetAllEmployees(int pageNumber = 0)
+        {
+            
+            using (IDbConnection con=new SqlConnection(connection))
+            {
+                int startIndex = (pageNumber * rowCount);
+                int endIndex = startIndex + rowCount;
+                if (employees != null)
+                {
+                    employees = con.Query<SampleEmployee>("Select * from StudentMarks").ToList();
+                }
+                return employees.Skip(startIndex).Take(rowCount).ToList();
+            }
+        }
+    }
+}
---
+++ API_Demo/Models/SampleEmployee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace API_Demo.Models
+{
+    public class SampleEmployee
+    {
+        public string Id { get; set; }
+      //  [Column("First Name")]
+        public string FirstName { get; set; }
+      //  [Column("Last Name")]
+        public string LastName { get; set; }
+        public decimal Marks { get; set; }
+    }
+}
